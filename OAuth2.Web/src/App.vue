32636ce5c47a77f370727ad75f6efad58e50fb23
liberty <wkdghcjf1234@gmail.com>
2026-07-11 00:25:14 +0900

    [Web] Remove HelloWorld component

--- OAuth2.Web/src/App.vue
+++ OAuth2.Web/src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import ThemeSelector from './core/ThemeSelector.vue';
-import HelloWorld from './components/HelloWorld.vue'
+import ThemeSelector from './core/components/ThemeSelector.vue';
 </script>
 
 <template>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <ThemeSelector />
-  <HelloWorld />
 </template>
